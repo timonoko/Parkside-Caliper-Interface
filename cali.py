aac466b705296cbb45c405bcfc370a358846c7c4
Timo Noko <timonoko@users.noreply.github.com>
2025-03-06 13:56:04 +0200

    Negative numbers

--- cali.py
+++ cali.py
@@ -1,18 +1,24 @@
 import time
 from machine import Pin
 
-led=Pin(2,Pin.OUT)
 clock=Pin(5,Pin.IN)
 data=Pin(4,Pin.IN)
-
+led=Pin(2,Pin.OUT)
 led.value(0)
+
 while True:
-    numero=0
     if clock.value()==0:
+        numero=0
         for z in range(20):
             while clock.value()==0: pass
             if data.value()==1: numero=numero|1<<z
             while clock.value()==1: pass
+        sign=0
+        for z in range(2):
+            while clock.value()==0: pass
+            if data.value()==1: sign=sign|1<<z
+            while clock.value()==1: pass
+        if sign==1: numero=-numero
         print(numero/100.)
         time.sleep(0.02)
         

61d4afd6583ee54a35cbdfe4c25e5bd00fd0ea3e
Timo Noko <timonoko@users.noreply.github.com>
2025-03-06 18:20:38 +0200

    äää

--- cali.py
+++ cali.py
@@ -6,6 +6,11 @@ data=Pin(4,Pin.IN)
 led=Pin(2,Pin.OUT)
 led.value(0)
 
+def almost_equal(num1, num2):
+    return abs(num1-num2)<100
+
+prevnum=17000
+
 while True:
     if clock.value()==0:
         numero=0
@@ -19,7 +24,10 @@ while True:
             if data.value()==1: sign=sign|1<<z
             while clock.value()==1: pass
         if sign==1: numero=-numero
-        print(numero/100.)
+        if numero==prevnum: pass
+        elif numero<16000 and almost_equal(prevnum,numero):
+            print(numero/100.)
+        prevnum=numero
         time.sleep(0.02)
         
     
